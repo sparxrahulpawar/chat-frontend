1e6f7b228bf6a6603b3693e0eac9a570a728fc50
sparxrahulpawar <sparxoneman@gmail.com>
2024-10-02 22:20:40 +0530

    Create a login and register page

--- src/layout/AuthLayout.js
+++ src/layout/AuthLayout.js
@@ -3,9 +3,6 @@ import React from "react";
 const AuthLayout = ({ children }) => {
   return (
     <>
-      {/* <header className="flex justify-center items-center py-3 h-20 shadow-md bg-white">
-        <img src="" alt="logo" width={180} height={60} />
-      </header> */}
       {children}
     </>
   );
